e8ce20955923f0862453e97c2fc1cb82a409b2d3
André Luiz <alps2@cin.ufpe.br>
2021-08-24 14:10:44 -0300

    Added a file to ease import plotting library

---
+++ imports.py
@@ -0,0 +1,10 @@
+import subprocess
+import sys
+
+# You can do this manually if you prefer
+# Or just run this script once to install necessary libraries
+
+def install(package):
+    subprocess.check_call([sys.executable, "-m", "pip", "install", package])
+
+install('matplotlib')
